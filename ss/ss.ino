68e22cd6de990dddf2d6d5f20dedf1c85ea2a77f
lofa40444 <as0921015717@gmail.com>
2017-05-01 19:14:30 +0800

    Add files via upload

---
+++ ss/ss.ino
@@ -0,0 +1,20 @@
+#include <LiquidCrystal.h>
+
+char c;
+LiquidCrystal lcd(12, 11, 5, 4, 3, 2); // initialize interface pins
+void setup() {
+  lcd.begin(16, 2);   // set up the LCD's number of columns and rows: 
+  Serial.begin(9600);
+
+}
+void loop() {
+  if(Serial.available() > 0)
+  {
+     c = Serial.read();  
+  
+    lcd.write(Serial.read()); 
+   
+   
+
+  }
+}
